091e62168e48e4b358e1b479f3657d3303f80a39
dev-jv <dev.javierleon@gmail.com>
2021-01-08 17:26:13 -0500

    seven

--- src/app/components/shared/navbar/navbar.component.ts
+++ src/app/components/shared/navbar/navbar.component.ts
@@ -3,7 +3,6 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-
 })
 export class NavbarComponent implements OnInit {
 
